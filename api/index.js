0ddd9589109af541825b7c63d8ca7d7aa0c420f8
Josue Flores <jflored@ucenfotec.ac.cr>
2018-07-03 22:11:04 -0600

    added package.json

--- api/index.js
+++ api/index.js
@@ -62,13 +62,13 @@ app.use( function(req, res, next) {
 /**
  * Exportams todas las rutas dentro del index.js
  */
-const usuarios = require('./components/users/users.route');
+const profesores = require('./components/profesores/profesores.route');
 
 
 /**
  * Le indicamos que le de acceso externo a las rutas inicializadas
  */
-app.use('/api', usuarios);
+app.use('/api', profesores);
 
 
 // Se guarda todo lo que se ha realizado
